a66629ad610fed17b31a46c6715e0841e5552f73
OSOFTWARE-123 <84440613+OSOFTWARE-123@users.noreply.github.com>
2022-08-30 22:03:10 +0300

    Erros

---
+++ Deneme/main.js
@@ -0,0 +1,9 @@
+var h1 = document.getElementById("error");
+
+function accepted() {
+    h1.innerHTML = "ERROR!!! Your IP Address Has Been Copied!!!";
+}
+
+function declined() {
+    h1.innerHTML = "ERROR!!! Your IP Address Has Been Not Copied!!!";
+}
