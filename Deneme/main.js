5aa4adccefe874f6abbd1a2fa556eb86727100db
OSOFTWARE-123 <84440613+OSOFTWARE-123@users.noreply.github.com>
2022-08-30 22:22:37 +0300

    JavaScript BTNs

--- Deneme/main.js
+++ Deneme/main.js
@@ -2,8 +2,12 @@ var h1 = document.getElementById("error");
 
 function accepted() {
     h1.innerHTML = "ERROR!!! Your IP Address Has Been Copied!!!";
+    document.getElementById("acc").disabled = true;
+    document.getElementById("dec").disabled = true;
 }
 
 function declined() {
     h1.innerHTML = "ERROR!!! Your IP Address Has Been Not Copied!!!";
+    document.getElementById("acc").disabled = true;
+    document.getElementById("dec").disabled = true;
 }
