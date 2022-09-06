9c47bfa93777d3d696a9ea51533d0c885e04402d
OSOFTWARE-123 <84440613+OSOFTWARE-123@users.noreply.github.com>
2022-09-06 15:32:50 +0300

    Error2

--- Deneme/main.js
+++ Deneme/main.js
@@ -1,4 +1,5 @@
 var h1 = document.getElementById("error");
+var h1_2 = document.getElementById("error2");
 
 function accepted() {
     h1.innerHTML = "ERROR!!! Your IP Address Has Been Copied!!!";
@@ -10,4 +11,16 @@ function declined() {
     h1.innerHTML = "ERROR!!! Your IP Address Has Been Not Copied!!!";
     document.getElementById("acc").disabled = true;
     document.getElementById("dec").disabled = true;
+}
+
+function accepted2() {
+    h1_2.innerHTML = "ERROR!!! Your IP Address Has Been Copied!!!";
+    document.getElementById("acc2").disabled = true;
+    document.getElementById("dec2").disabled = true;
+}
+
+function declined2() {
+    h1_2.innerHTML = "ERROR!!! Your IP Address Has Been Not Copied!!!";
+    document.getElementById("acc2").disabled = true;
+    document.getElementById("dec2").disabled = true;
 }
